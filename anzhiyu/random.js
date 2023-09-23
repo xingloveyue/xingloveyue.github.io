0fbece5b82acf03d75cf0462002d00bda5c41202
xingloveyue <2387113969@qq.com>
2023-09-23 10:47:30 +0800

    Site updated: 2023-09-23 10:47:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/19/hello-world/","2023/09/22/Windows/DOC命令/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/22/Windows/DOC命令/","2023/09/23/Windows/批处理/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
